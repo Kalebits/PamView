39d3d524be6ac12113e4d21fadeb4e92b5d1f90b
Ana-Kaleb <Ana-kaleb@Example.com>
2023-11-06 08:47:38 -0300

    Map view

---
+++ AppRpgEtec/ViewModels/Usuarios/LocalizacaoViewModel.cs
@@ -0,0 +1,14 @@
+using Javax.Security.Auth;
+using System.Net.NetworkInformation;
+using Map = Microsoft.Maui.Controls.Maps.Map;
+
+namespace AppRpgEtec.ViewModels.Usuarios;
+
+public class LocalizacaoViewModel : ContentView
+{
+    public LocalizacaoViewModel()
+    {
+        
+    }
+}
+
